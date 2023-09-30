0a40de836a8ffb62955ee25c74297a242ea80005
AL-Amin008 <mamin.193008@bscse.uiu.ac.bd>
2023-09-30 20:37:25 +0600

    change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const app = require('./app');
+const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const express = require('express');
+const app = require('./app'); // Import the 'app' instance created earlier
 
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.MONGODB_URL || 'mongodb://localhost:27017/Assignment';
 
@@ -14,9 +14,7 @@ mongoose
   })
   .then(() => {
     console.log('Connected to MongoDB successfully');
-    
-    
-    const app = require('./app'); 
+
     const PORT = process.env.RUNNING_PORT || 8080;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
@@ -25,6 +23,3 @@ mongoose
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
-
-
-
